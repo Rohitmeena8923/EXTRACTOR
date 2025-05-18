dd0a1dc5dcc661383b327f3786d9a9143a9518dd
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-05-18 11:26:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import threading
+from Extractor import __main__  # Ensure this runs the bot
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from john  wick'
+    return 'Hello from john wick'
 
+def run_bot():
+    __main__.main()  # Or whatever the bot start function is called
 
 if __name__ == "__main__":
-    app.run()
+    threading.Thread(target=run_bot).start()
+    app.run(host="0.0.0.0", port=5000)
